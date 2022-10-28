b65fc7228106ab835433f14a6b0d34f3f5908561
Kevin Harold Gutierrez Ramirez <harold.dev1993@gmail.com>
2022-10-28 17:54:29 -0500

    entendiendo callbacks

---
+++ src/callback/index.js
@@ -0,0 +1,21 @@
+function sum(num1, num2) {
+    return num1 + num2
+}
+
+function calc(num1, num2, callback) {
+    return callback(num1, num2)
+}
+
+console.log(calc(5, 5, sum))
+
+setTimeout(function () {
+    console.log('Hello JavaScript')
+}, 5000)
+
+
+function greeting(name) {
+    console.log(`Hello ${name}`)
+}
+
+setTimeout(greeting, 2000, 'Kegura')
+
