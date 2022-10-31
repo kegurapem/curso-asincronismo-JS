43f37f2d3fa5dd60ea6c240b86451516b668b5aa
Kevin Harold Gutierrez Ramirez <harold.dev1993@gmail.com>
2022-10-31 14:54:48 -0500

    avanzando promesas

---
+++ src/promise/index.js
@@ -0,0 +1,19 @@
+const promise = new Promise(function (resolve, reject) {
+    resolve('Todo ok')
+})
+
+const cows = 15
+
+const countCows = new Promise(function (resolve, reject) {
+    if (cows > 10) {
+        resolve(`We have ${cows} cown on the farm`)
+    } else {
+        reject('There is no cowns on the farm')
+    }
+})
+
+countCows.then((result) => {
+    console.log(result)
+}).catch((error) => {
+    console.log(error)
+}).finally(() => console.log('Finally'))
